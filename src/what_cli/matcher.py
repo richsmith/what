2ac5b19c2af1bbc8c0988646661f09a724d15019
Rich Smith <rls@hwyl.org>
2025-05-06 13:51:04 +0100

    Add matching comment

--- src/what_cli/matcher.py
+++ src/what_cli/matcher.py
@@ -6,6 +6,8 @@ from .entities import Entity, File, Process, User
 
 def match(name) -> Entity:
 
+    # Match entities in the order in which we expect to be most
+    # commonly used
     for entity_type in (File, Process, User):
         if entity := entity_type.match(name):
             return entity
